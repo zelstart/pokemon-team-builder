2c20d62ebe89683d11ac2eac6f2700d2d567dad7
zelstart <e.pomeroy95@gmail.com>
2023-12-04 18:37:46 -0500

    pokemon card element (html + css only)

--- client/src/components/PokemonCard/PokemonCard.jsx
+++ client/src/components/PokemonCard/PokemonCard.jsx
@@ -1,62 +1,63 @@
 import React from 'react';
 import { Card, Row, Col, Form, Button, Container, Modal } from 'react-bootstrap';
+import './PokemonCard.css';
+
+// this is just the html so far!! need to actually make it dynamic with props and such
 
 function PokemonCard() {
     return (
-        <Container>
-            <Row>
-                <Col className='poke-card'>
+<Row className='justify-content-center'>
+    <Col lg={3} md={6} sm={12} className='poke-card m-1'>
 
-                    <Row className='card-top'>
-                        <Col lg={8}>
-                            <p className='poke-name'>Bulbasaur</p>
-                        </Col>
+        {/* CARD TOP // NAME + LEVEL */}
+        <Row className='card-top justify-content-center'>
+            <Col lg={6} md={6} sm={6} className='d-flex align-items-center'>
+                <p className='poke-name'>bulbasaur</p>
+            </Col>
+            <Col lg={3} md={3} sm={3} className='d-flex align-items-center'>
+                <p className='poke-level'>lv. 50</p>
+            </Col>
+        </Row>
 
-                        <Col lg={2}>
-                            <p className='poke-level'>lv. 50</p>
-                        </Col>
-                    </Row>
+        {/* CARD MIDDLE // MOVESET + SPRITE */}
+        <Row className='card-middle'>
+            <Col lg={12}>
+                <p className='moves bold'>moveset</p>
+            </Col>
+            <Col lg={4} md={4} sm={4}>
+                <Row>
+                    <p className='poke-move'>vine whip</p>
+                    <p className='poke-move'>leer</p>
+                </Row>
+            </Col>
+            <Col lg={4} md={4} sm={4}>
+                <Row>
+                    <p className='poke-move'>razor leaf</p>
+                            <p className='poke-move'>growl</p>
+                        </Row>
+                    </Col>
+                    <Col lg={4}>
+                        <img className='poke-icon' src='https://img.pokemondb.net/sprites/sword-shield/icon/bulbasaur.png' alt='bulbasaur' />
+                    </Col>
+                </Row>
 
-                    <Row className='card-middle'>
-                        <Col lg={8}>
-                            <Row>
-                                <Col lg={6}>
-                                    <p className='poke-move'>vine whip</p>
-                                </Col>
-                                <Col lg={6}>
-                                    <p className='poke-move'>leer</p>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col lg={6}>
-                                    <p className='poke-move'>razor leaf</p>
-                                </Col>
-                                <Col lg={6}>
-                                    <p className='poke-move'>growl</p>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col lg={4}>
-                            <Row>
-                                <Col lg={12}>
-                                    <img className='poke-icon' src='https://img.pokemondb.net/sprites/sword-shield/icon/bulbasaur.png' alt='bulbasaur' />
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
+                {/* CARD BOTTOM // ABILITY + NATURE */}
+                <Row className='card-bottom'>
+                    <Col lg={6}>
+                        <p className='ability'><span className="bold">ability: </span>overgrow</p>
+                        <p className='nature'><span className="bold">nature: </span>modest</p>
+                    </Col>
 
+                    <Col lg={6} className='types d-flex flex-column'>
+                        <img className='type-icon' src='https://archives.bulbagarden.net/media/upload/thumb/7/7b/GrassIC_SV.png/105px-GrassIC_SV.png' alt='grass' />
+                        <img className='type-icon' src='https://archives.bulbagarden.net/media/upload/thumb/7/7b/GrassIC_SV.png/105px-GrassIC_SV.png' alt='grass' />
+                    </Col>
+                    
+                </Row>
 
-                    <Row className='card-bottom'>
-                        <Col lg={8}>
-                            <p><span className="bold">ability: </span>overgrow</p>
-                            <p><span className="bold">nature: </span>modest</p>
-                        </Col>
-                    </Row>
-
-
-                </Col>
-            </Row>
-        </Container>
+                    <div className='stat-button'>stats <FontAwesomeIcon icon={faChevronRight} className='stats-chevron'/></div>
+            </Col>
+        </Row>
     )
 }
 export default PokemonCard;
