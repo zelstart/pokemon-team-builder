6bc6e0984d0a238dddfbdb1d69ea701bb3a8da5a
zelstart <e.pomeroy95@gmail.com>
2023-12-04 15:23:52 -0500

    setting up app/main routers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from '.components/z_pages/Home.jsx';
+import CreateTeam from './components/z_pages/CreateTeam.jsx';
+import Me from './components/z_pages/Me.jsx';
+import Profile from './components/z_pages/Profile.jsx';
+import TeamView from './components/z_pages/TeamView.jsx';
+import NotFound from './components/z_pages/NotFound.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/me',
+        element: <Me />
+      }, {
+        path: '/profile/:id',
+        element: <Profile />
+      }, {
+        path: '/create-team',
+        element: <CreateTeam />
+      }, {
+        path: '/team/:id',
+        element: <TeamView />
+      }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
