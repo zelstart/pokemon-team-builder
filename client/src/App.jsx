e7e84da6a25cf692757d90c995c50316865f2f7c
zelstart <e.pomeroy95@gmail.com>
2023-12-04 15:25:22 -0500

    removed unnecessary imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
