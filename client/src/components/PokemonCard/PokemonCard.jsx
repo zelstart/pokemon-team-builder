053578c2ce6cda8b752f09bc86afd10ee4e25049
zelstart <e.pomeroy95@gmail.com>
2023-12-05 18:56:54 -0500

    stats card basically done

--- client/src/components/PokemonCard/PokemonCard.jsx
+++ client/src/components/PokemonCard/PokemonCard.jsx
@@ -14,15 +14,29 @@ function PokemonCard() {
     const handleFlip = () => {
         setIsFlipped(!isFlipped);
     };
-
+    
+    const MAX_HP_STAT_VALUE = 720;
+    const MAX_OTHER_STAT_VALUE = 250;
+    
     // TEMPORARY DATA
+    const pokemonLevel = 50; // Placeholder for Pokemon's level
     const [baseStats, setBaseStats] = useState({ hp: 45, atk: 49, def: 49, spa: 65, spd: 65, spe: 45 });
     const [ivStats, setIvStats] = useState({ hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: 31 });
     const [evStats, setEvStats] = useState({ hp: 252, atk: 0, def: 0, spa: 252, spd: 0, spe: 4 });
 
-    // const [baseStats, setBaseStats] = useState({ hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0 });
-    // const [ivStats, setIvStats] = useState({ hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0 });
-    // const [evStats, setEvStats] = useState({ hp: 0, atk: 0, def: 0, spa: 0, spd: 0, spe: 0 });
+    // const [baseStats, setBaseStats] = useState(baseStatsProp);
+    // const [ivStats, setIvStats] = useState(ivStatsProp);
+    // const [evStats, setEvStats] = useState(evStatsProp);
+
+
+    const stats = [
+        { name: 'HP', base: baseStats.hp, iv: ivStats.hp, ev: evStats.hp },
+        { name: 'Atk', base: baseStats.atk, iv: ivStats.atk, ev: evStats.atk },
+        { name: 'Def', base: baseStats.def, iv: ivStats.def, ev: evStats.def },
+        { name: 'SpA', base: baseStats.spa, iv: ivStats.spa, ev: evStats.spa },
+        { name: 'SpD', base: baseStats.spd, iv: ivStats.spd, ev: evStats.spd },
+        { name: 'Spe', base: baseStats.spe, iv: ivStats.spe, ev: evStats.spe },
+    ];
 
     // will need to pass in props for baseStats, ivStats, evStats
     // useEffect(() => {
@@ -30,10 +44,6 @@ function PokemonCard() {
     //     setIvStats(ivStatsProp);
     //     setEvStats(evStatsProp);
     // }, [baseStatsProp, ivStatsProp, evStatsProp]);
-
-    const MAX_HP_STAT_VALUE = 720;
-    const MAX_OTHER_STAT_VALUE = 250;
-    const pokemonLevel = 50; // Placeholder for Pokemon's level
 
     // calculate the stats for the pokemon
     const calculateTotalStats = (stat) => {
@@ -56,6 +66,26 @@ function PokemonCard() {
         return `rgb(${red}, ${green}, 0)`;
     };
 
+
+    const StatRow = ({ statName, baseStat, iv, ev }) => {
+        const totalStat = calculateTotalStats(statName.toLowerCase());
+        const width = totalStat / (statName === 'HP' ? MAX_HP_STAT_VALUE : MAX_OTHER_STAT_VALUE) * 100;
+        const color = calculateColor(statName.toLowerCase());
+
+        return (
+            <Row className='stat-table'>
+                <div className='d-flex'>
+                    <Col className='rc-400-bold'>{statName}</Col>
+                    <Col className='rc-400'>{baseStat}</Col>
+                    <Col className='rc-400'>{iv}</Col>
+                    <Col className='rc-400'>{ev}</Col>
+                    <Col className='rc-400 d-flex align-items-center'>
+                        <div className='stat-bar' style={{ width: `${width}%`, backgroundColor: color }}></div>
+                    </Col>
+                </div>
+            </Row>
+        );
+    };
 
     // i'm thinking the best/easiest way is going to be make each field on the card editable. maybe add a little edit
     // icon, and when you click it, it turns into a form field. then when you click out of it, it turns back into text
@@ -81,83 +111,18 @@ function PokemonCard() {
 
                         {/* CARD MIDDLE // STATS */}
                         {/* This will eventually be condensed into a map function for each stat instead of making a new row for each stat.*/}
-                        <Row className='card-middle'>
-                            <Col className='stats' lg={12}>
-                                <div className='stat'>
-                                    <div className='stat-line'>
-                                        <span className='bold'>HP</span>
-                                        <span>{calculateTotalStats('hp')}</span>
-                                        <div className='stat-bar' style={{ width: `${calculateTotalStats('hp') / MAX_HP_STAT_VALUE * 100}%`, backgroundColor: calculateColor('hp') }}></div>
-                                    </div>
-                                </div>
-
-                                <div className='stat'>
-                                    <div className='stat-line'>
-                                        <span className='bold'>Atk</span>
-                                        <span>{calculateTotalStats('atk')}</span>
-                                        <div className='stat-bar' style={{ width: `${calculateTotalStats('atk') / MAX_HP_STAT_VALUE * 100}%`, backgroundColor: calculateColor('atk') }}></div>
-                                    </div>
-                                </div>
-
-                                <div className='stat'>
-                                    <div className='stat-line'>
-                                        <span className='bold'>Def</span>
-                                        <span>{calculateTotalStats('def')}</span>
-                                        <div className='stat-bar' style={{ width: `${calculateTotalStats('def') / MAX_HP_STAT_VALUE * 100}%`, backgroundColor: calculateColor('def') }}></div>
-                                    </div>
-                                </div>
-
-                                <div className='stat'>
-                                    <div className='stat-line'>
-                                        <span className='bold'>SpA </span>
-                                        <span>{calculateTotalStats('spa')}</span>
-                                        <div className='stat-bar' style={{ width: `${calculateTotalStats('spa') / MAX_HP_STAT_VALUE * 100}%`, backgroundColor: calculateColor('spa') }}></div>
-                                    </div>
-                                </div>
-
-
-                                <div className='stat'>
-                                    <div className='stat-line'>
-                                        <span className='bold'>SpD </span>
-                                        <span>{calculateTotalStats('spd')}</span>
-                                        <div className='stat-bar' style={{ width: `${calculateTotalStats('spd') / MAX_HP_STAT_VALUE * 100}%`, backgroundColor: calculateColor('spd') }}></div>
-                                    </div>
-                                </div>
-
-
-                                <div className='stat'>
-                                    <div className='stat-line'>
-                                        <span className='bold'>Spe </span>
-                                        <span>{calculateTotalStats('def')}</span>
-                                        <div className='stat-bar' style={{ width: `${calculateTotalStats('spe') / MAX_HP_STAT_VALUE * 100}%`, backgroundColor: calculateColor('def') }}></div>
-                                    </div>
-                                </div>
-
-                            </Col>
+                        <Row className='stat-table'>
+                            <div className='d-flex'>
+                                <Col className='rc-400-bold'>Stat</Col>
+                                <Col className='rc-400-bold'>Base</Col>
+                                <Col className='rc-400-bold'>IV</Col>
+                                <Col className='rc-400-bold'>EV</Col>
+                                <Col></Col>
+                            </div>
                         </Row>
-
-                        {/* CARD BOTTOM // IVs + EVs */}
-                        {/* will clean/pretty this up later. eventually will fill data dynamically.  */}
-                        <Row className='card-bottom'>
-                            <Col className='iv-spread' lg={6}>
-                                <p className='bold'>IVs</p>
-                                <p className='stat'><span className='bold'>HP: </span> 0 </p>
-                                <p className='stat'><span className='bold'>Atk: </span> 0 </p>
-                                <p className='stat'><span className='bold'>Def: </span> 0 </p>
-                                <p className='stat'><span className='bold'>SpA: </span> 0 </p>
-                                <p className='stat'><span className='bold'>SpD: </span> 0 </p>
-                                <p className='stat'><span className='bold'>Spe: </span> 0 </p>
-                            </Col>
-                            <Col className='ev-spread' lg={6}>
-                                <p className='bold'>EVs</p>
-                                <p className='stat'><span className='bold'>HP: </span> 0 </p>
-                                <p className='stat'><span className='bold'>Atk: </span> 0 </p>
-                                <p className='stat'><span className='bold'>Def: </span> 0 </p>
-                                <p className='stat'><span className='bold'>SpA: </span> 0 </p>
-                                <p className='stat'><span className='bold'>SpD: </span> 0 </p>
-                                <p className='stat'><span className='bold'>Spe: </span> 0 </p>
-                            </Col>
-                        </Row>
+                        {stats.map((stat) => (
+                            <StatRow key={stat.name} statName={stat.name} baseStat={stat.base} iv={stat.iv} ev={stat.ev} />
+                        ))}
                     </Col>
                 </Row>
             ) : (
@@ -179,18 +144,18 @@ function PokemonCard() {
                         {/* CARD MIDDLE // MOVESET + SPRITE */}
                         <Row className='card-middle'>
                             <Col lg={12}>
-                                <p className='moves bold'>moveset</p>
+                                <p className='rc-400 rc-400-bold'>moveset</p>
                             </Col>
                             <Col lg={4} md={4} sm={4}>
                                 <Row>
-                                    <p className='poke-move'>vine whip</p>
-                                    <p className='poke-move'>leer</p>
+                                    <p className='rc-400'>vine whip</p>
+                                    <p className='rc-400'>leer</p>
                                 </Row>
                             </Col>
                             <Col lg={4} md={4} sm={4}>
                                 <Row>
-                                    <p className='poke-move'>razor leaf</p>
-                                    <p className='poke-move'>growl</p>
+                                    <p className='rc-400'>razor leaf</p>
+                                    <p className='rc-400'>growl</p>
                                 </Row>
                             </Col>
                             <Col lg={4}>
@@ -201,8 +166,8 @@ function PokemonCard() {
                         {/* CARD BOTTOM // ABILITY + NATURE */}
                         <Row className='card-bottom'>
                             <Col lg={6}>
-                                <p className='ability'><span className="bold">ability: </span>overgrow</p>
-                                <p className='nature'><span className="bold">nature: </span>modest</p>
+                                <p><span className="rc-400-bold">ability: </span><span className='rc-400'>overgrow</span></p>
+                                <p><span className="rc-400-bold">nature: </span><span className='rc-400'>modest</span></p>
                             </Col>
 
                             <Col lg={6} className='types d-flex flex-column'>
@@ -215,6 +180,8 @@ function PokemonCard() {
                 </Row>
             )}
         </div>
+
     );
 }
+
 export default PokemonCard;
