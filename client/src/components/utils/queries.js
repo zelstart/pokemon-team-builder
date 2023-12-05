55c6daaa917797c74c1cffa5bccec18e37ce2685
unknown <blancardt@merrimack.edu>
2023-12-04 20:30:44 -0500

    added the utils, in for the frontend to use.

---
+++ client/src/components/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
