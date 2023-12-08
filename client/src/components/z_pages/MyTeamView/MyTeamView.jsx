dac57b6f373bb0cfa868e8f3357bb154d263c4d2
zelstart <e.pomeroy95@gmail.com>
2023-12-08 16:38:40 -0500

    renamed

---
+++ client/src/components/z_pages/MyTeamView/MyTeamView.jsx
@@ -0,0 +1,9 @@
+import { React, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const MyTeamView = () => { 
+return (
+    <div></div>
+)}
+
+export default MyTeamView;
