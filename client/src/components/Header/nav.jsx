544be4e97546e633d5c44ce115a3e011436a36f0
zelstart <e.pomeroy95@gmail.com>
2023-12-08 16:14:59 -0500

    reformatted with prettier

--- client/src/components/Header/nav.jsx
+++ client/src/components/Header/nav.jsx
@@ -1,98 +1,92 @@
-import { useState } from 'react';
-import { Link , useLocation } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from '../SignupForm/SignupForm.jsx';
-import LoginForm from '../LoginForm/LoginForm.jsx';
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import SignUpForm from "../SignupForm/SignupForm.jsx";
+import LoginForm from "../LoginForm/LoginForm.jsx";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
-import pokeDex from '../../assets/pokeDex.png'
+import pokeDex from "../../assets/pokeDex.png";
 
-import './nav.css';
+import "./nav.css";
 
-function Header () {
-    // set modal display state
-    const [showModal, setShowModal] = useState(false);
+function Header() {
+  // set modal display state
+  const [showModal, setShowModal] = useState(false);
 
-    const currentPage = useLocation().pathname;
+  const currentPage = useLocation().pathname;
 
-    return (
-        <>
-            <nav className='d-flex pt-3'>
+  return (
+    <>
+      <nav className="d-flex pt-3">
+        <img className="ps-5" src={pokeDex} alt="A pokedex outline!" />
 
-                <img className = "ps-5"src={pokeDex} alt="A pokedex outline!" />
+        <section id="" className="d-flex flex-column">
+          <section id="name" className="d-flex align-items-center ps-3">
+            <h1 id="title">Pokemon Team Builder</h1>
+          </section>
 
-                <section id =  "" className='d-flex flex-column'>
+          <section id="links" className="d-flex align-items-center ps-3">
+            <Link to="/" className="nav-link d-flex align-items-center">
+              <span className={currentPage === "/" ? "icon" : ""}></span>Home
+            </Link>
 
-                    <section id='name' className= 'd-flex align-items-center ps-3'>
+            <Link
+              to="/teams"
+              className="nav-link d-flex align-items-center px-3"
+            >
+              <span className={currentPage === "/teams" ? "icon" : ""}></span>
+              Teams
+            </Link>
 
-                        <h1 id='title'>Pokemon Team Builder</h1>
-
-                    </section>
-
-                    <section id='links' className='d-flex align-items-center ps-3'>
-
-                    <Link to="/"
-                    
-                        className='nav-link d-flex align-items-center'
-                    
-                    ><span className={currentPage === '/' ? 'icon' : ''}></span>Home</Link> 
-
-                    <Link to="/teams"
-                    
-                    className='nav-link d-flex align-items-center px-3'
-                    
-                    ><span className={currentPage === '/teams' ? 'icon' : ''}></span>Teams</Link> 
-
-                        
-                        {/* if user is logged in show logout button */}
-                        {Auth.loggedIn() ? (
-                            <Nav.Link className='nav-link pe-2' onClick={Auth.logout}>Logout</Nav.Link>
-                        ) : (
-                            <Nav.Link className='nav-link pe-2' onClick={() => setShowModal(true)}>Login</Nav.Link>
-                        )}
-
-
-                    </section>
-
-                </section>
-
-            </nav>
-            {/* set modal data up for logging in */}
-            <Modal
-                size='lg'
-                show={showModal}
-                onHide={() => setShowModal(false)}
-                aria-labelledby='signup-modal'>
-                {/* tab container to do either signup or login component */}
-                <Tab.Container defaultActiveKey='login'>
-                <Modal.Header closeButton>
-                    <Modal.Title id='signup-modal'>
-                    <Nav variant='pills'>
-                        <Nav.Item>
-                        <Nav.Link eventKey='login'>Login</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                        <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Tab.Content>
-                    <Tab.Pane eventKey='login'>
-                        <LoginForm handleModalClose={() => setShowModal(false)} />
-                    </Tab.Pane>
-                    <Tab.Pane eventKey='signup'>
-                        <SignUpForm handleModalClose={() => setShowModal(false)} />
-                    </Tab.Pane>
-                    </Tab.Content>
-                </Modal.Body>
-                </Tab.Container>
-            </Modal>
-        </>
-
-    )
+            {/* if user is logged in show logout button */}
+            {Auth.loggedIn() ? (
+              <a className="nav-link pe-2" onClick={Auth.logout}>
+                Logout
+              </a>
+            ) : (
+              <a className="nav-link pe-2" onClick={() => setShowModal(true)}>
+                Login/Signup
+              </a>
+            )}
+          </section>
+        </section>
+      </nav>
+      {/* set modal data up for logging in */}
+      <Modal
+        size="lg"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">
+              <Nav variant="pills">
+                <Nav.Item>
+                  <Nav.Link eventKey="login">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="signup">
+                <SignUpForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
+  );
 }
 
 export default Header;
