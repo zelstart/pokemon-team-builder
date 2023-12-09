169044b7409061d22f2920a02d036e1c29f91016
zelstart <e.pomeroy95@gmail.com>
2023-12-09 17:10:46 -0500

    trying to remove all traces of react-select

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-select'],
-  },
     server: {
     port: 3000,
     open: true,
