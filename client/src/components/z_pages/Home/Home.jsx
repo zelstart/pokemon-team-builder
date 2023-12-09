4ccab14f7ff9650af4291999cc35cf7723d683aa
zelstart <e.pomeroy95@gmail.com>
2023-12-08 21:41:57 -0500

    home page populates team cards (temp data for now)

--- client/src/components/z_pages/Home/Home.jsx
+++ client/src/components/z_pages/Home/Home.jsx
@@ -1,9 +1,40 @@
 import { React, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import TeamCard from '../../TeamCard/TeamCard.jsx';
 
-const Home = () => { 
-return (
-    <div></div>
-)}
+const Home = () => {
+
+    //placeholder data for team card
+    const teamData = {
+        teamName: "Team Name Here",
+        // rating: 4.5,
+        pokemon: [
+            { name: "Pikachu", icon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+            { name: "Pikachu", icon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+            { name: "Pikachu", icon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+            { name: "Pikachu", icon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+            { name: "Pikachu", icon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+            { name: "Pikachu", icon: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+        ],
+        user: "User1",
+        creator: "Creator1"
+    };
+
+    return (
+        <Container className='mt-2'>
+            <Row className='mx-5 mt-5'>
+                <Col lg={12}>
+                    <h2>recently created teams</h2>
+                </Col>
+            </Row>
+
+            <Row className='mx-5'>
+
+                    <TeamCard {...teamData} />
+
+            </Row>
+        </Container>
+    )
+}
 
 export default Home;
