234ad495daac1983a5c37b5c0be2f2fa38c125a0
unknown <blancardt@merrimack.edu>
2023-12-10 15:11:44 -0500

    added appropriate mutations and queries to the resolver.js file.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Teams } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
@@ -11,6 +11,16 @@ const resolvers = {
         return User.findOne({ _id: context.user._id });
       }
       throw AuthenticationError;
+    },
+    userTeams: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Teams.find(params).sort({ createdAt: -1 });
+    },
+    teams: async (parent, {}) => {
+      return Teams.find().sort({ createdAt: -1 });
+    },
+    team: async (parent, { teamId }) => {
+      return Teams.findOne({ _id: teamId });
     },
   },
   Mutation: {
@@ -35,6 +45,103 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
+    },
+    createTeam: async (parent, {}, context) => {
+      if (context.user) {
+        const team = await Teams.create({
+          userCreator: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { teams: team._id } }
+        );
+
+        return team;
+      }
+      throw AuthenticationError;
+    },
+    updateTeam: async (parent, { teamId, name }, context) => {
+      if (context.user) {
+        return Teams.findOneAndUpdate(
+          { _id: teamId, userCreator: context.user.username },
+          { $set: {name: name}, },
+          { new: true, }
+        );
+      }
+      throw AuthenticationError;
+    },
+    removeTeam: async (parent, { teamId }, context) => {
+      if (context.user) {
+        const team = await Teams.findOneAndDelete({
+          _id: teamId,
+          userCreator: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { teams: team._id } }
+        );
+
+        return team;
+      }
+      throw AuthenticationError;
+    },
+    addPokemon: async (parent, { teamId, pokeDexNo }, context) => {
+      if (context.user) {
+        const pokemon = {
+          pokeDexNo: pokeDexNo
+        }
+        return Teams.findOneAndUpdate(
+          { _id: teamId, userCreator: context.user.username },
+          { $push: { pokemon: pokemon } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw AuthenticationError;
+    },
+    updatePokemon: async (parent, { teamId, pokeId, level, move_1, move_2, move_3, move_4, ability, item, hp_ev, attack_ev, special_attack_ev, defense_ev, special_defense_ev, speed_ev, attack_iv, special_attack_iv, defense_iv, special_defense_iv, speed_iv }, context) => {
+      if (context.user) {
+        // Find and update the team in the database
+        return Teams.findOneAndUpdate(
+          { teamId: teamId, userCreator: context.user.username, 'pokemon.pokeId': pokeId },
+          {
+            $set: {
+              'pokemon.$.level': level,
+              'pokemon.$.move_1': move_1,
+              'pokemon.$.move_2': move_2,
+              'pokemon.$.move_3': move_3,
+              'pokemon.$.move_4': move_4,
+              'pokemon.$.ability': ability,
+              'pokemon.$.item': item,
+              'pokemon.$.hp_ev': hp_ev,
+              'pokemon.$.attack_ev': attack_ev,
+              'pokemon.$.special_attack_ev': special_attack_ev,
+              'pokemon.$.defense_ev': defense_ev,
+              'pokemon.$.special_defense_ev': special_defense_ev,
+              'pokemon.$.speed_ev': speed_ev,
+              'pokemon.$.hp_iv': hp_iv,
+              'pokemon.$.attack_iv': attack_iv,
+              'pokemon.$.special_attack_iv': special_attack_iv,
+              'pokemon.$.defense_iv': defense_iv,
+              'pokemon.$.special_defense_iv': special_defense_iv,
+              'pokemon.$.speed_iv': speed_iv,
+            },
+          },
+          { new: true }
+        );      
+      }
+      throw AuthenticationError;
+    },
+    removePokemon: async (parent, { teamId, pokeId }, context) => {
+      if (context.user) {
+        return Teams.findOneAndUpdate(
+          { _id: teamId, userCreator: context.user.username },
+          { $pull: { pokemon: {_id: pokeId} } },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,10 +9,12 @@ const typeDefs = `
   type Teams {
     _id: ID
     name: String
+    userCreator: String
     pokemon: [Pokemon]
   }
 
   type Pokemon {
+    _id: ID
     pokeDexNo: Number
     level: Number
     move_1: String
@@ -42,7 +44,8 @@ const typeDefs = `
 
   type Query {
     user(username: String!): User
-    teams(username: String!): [Teams]
+    userTeams(username: String!): [Teams]
+    teams(): [Teams]
     team(teamId: ID!): Team
     me: User
   }
@@ -51,10 +54,11 @@ const typeDefs = `
     login(username: String!, password: String!): Auth
     addUser(username: String!, password: String!): Auth
     createTeam(): Teams
+    updateTeam(teamId: ID!, name: String!): Teams
     removeTeam(teamId: ID!): Teams
-    addPokemon(pokeDexNo: Number!): Teams
-    removePokemon(pokeId: ID!): Teams
-    updatePokemon(pokeId: ID!, level: Number!, move_1: String!, move_2: String!, move_3: String!, move_4: String!, ability: String!, item: String!, hp_ev: Number!, attack_ev: Number!, special_attack_ev: Number!, defense_ev: Number!, special_defense_ev: Number!, speed_ev: Number!, hp_iv: Number!, attack_iv: Number!, special_attack_iv: Number!, defense_iv: Number!, special_defense_iv: Number!, speed_iv: Number!): Teams
+    addPokemon(teamId: ID!, pokeDexNo: Number!): Teams
+    removePokemon(teamId: ID!, pokeId: ID!): Teams
+    updatePokemon(teamId: ID!, pokeId: ID!, level: Number!, move_1: String!, move_2: String!, move_3: String!, move_4: String!, ability: String!, item: String!, hp_ev: Number!, attack_ev: Number!, special_attack_ev: Number!, defense_ev: Number!, special_defense_ev: Number!, speed_ev: Number!, hp_iv: Number!, attack_iv: Number!, special_attack_iv: Number!, defense_iv: Number!, special_defense_iv: Number!, speed_iv: Number!): Teams
   }
 `;
 
