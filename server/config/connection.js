cabc2d118a2b183bcf8fa0393dfc8de3092f5d89
zelstart <e.pomeroy95@gmail.com>
2023-12-10 22:23:05 -0500

    changed connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams');
-
-// module.exports = mongoose.connection;
-
-
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams';
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams');
 
 module.exports = mongoose.connection;
+
+
+// const mongoose = require('mongoose');
+
+// const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams';
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true });
+
+// module.exports = mongoose.connection;
