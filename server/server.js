1e6e2cfdff8caa4c9699c7ea74a375159d9c73b2
zelstart <e.pomeroy95@gmail.com>
2023-12-10 23:01:16 -0500

    updated to serve static assets from dist not build

--- server/server.js
+++ server/server.js
@@ -26,12 +26,12 @@ const startApolloServer = async () => {
   app.use(express.json());
   app.use('/graphql', expressMiddleware(server));
   app.use(routes);
-  // if we're in production, serve client/build as static assets
+  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
     });
   }
   db.once('open', () => {
