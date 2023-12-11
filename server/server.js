e800b68ec7880251d8dfa0973e25f7caf991eabc
zelstart <e.pomeroy95@gmail.com>
2023-12-10 22:56:36 -0500

    added catch all route handler

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ const startApolloServer = async () => {
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
   }
   db.once('open', () => {
     app.listen(PORT, () => {
