8eb2f2b717fae777e93ee2fa0246fab1d9a7796c
zelstart <e.pomeroy95@gmail.com>
2023-12-10 22:40:50 -0500

    put the uri in a const

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams', {
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams';
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true,
-});
+  useUnifiedTopology: true
+})
+.then(() => console.log('Database connected successfully'))
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
 
