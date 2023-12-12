c2f859fbecc4287254e6df808ee2488e76ed2503
zelstart <e.pomeroy95@gmail.com>
2023-12-11 20:53:40 -0500

    initial deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,53 +1,10 @@
-
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import App from './App.jsx';
-// uncomment these as components/pages are created
-import Home from './components/z_pages/Home/Home.jsx';
-import CreateTeam from './components/z_pages/CreateTeam/CreateTeam.jsx';
-import MyTeamView from './components/z_pages/MyTeamView/MyTeamView.jsx';
-// import Me from './components/z_pages/Me.jsx';
-// import Profile from './components/z_pages/Profile.jsx';
-// import NotFound from './components/z_pages/NotFound.jsx';
-
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
- //   errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: '/me',
-        element: <MyTeamView />
-      }, 
-      // {
-      //   path: '/profile/:id',
-      //   element: <Profile />
-      // }, 
-      {
-        path: '/create-team',
-        element: <CreateTeam />
-      }, 
-      // {
-      //   path: '/team/:id',
-      //   element: <TeamView />
-      // }, {
-      //   path: '/test',
-      //   element: <Test />
-      // }
-    ],
-  },
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-
-);
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
