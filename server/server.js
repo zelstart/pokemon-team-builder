428ab008713da7cba07da881e47b99cebc64e4fc
zelstart <e.pomeroy95@gmail.com>
2023-12-12 18:03:03 -0500

    making changes nicole suggested

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-const db = require('./config/connection');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 const cors = require('cors');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 // start Apollo server
 const server = new ApolloServer({
   typeDefs,
@@ -19,18 +19,17 @@ const server = new ApolloServer({
     credentials: true // Credentials are allowed
   }
 });
-
-server.start().then(() => {
+const startApolloServer = async () => {
+  await server.start();
   app.use(cors());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  server.applyMiddleware({ app, path: '/graphql' }); // Apply Apollo middleware to the '/graphql' path
+  app.use('/graphql', expressMiddleware(server));
   app.use(routes);
-  
   // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
-    
+  
     // add a wildcard route to serve up the client's index.html file
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -40,7 +39,9 @@ server.start().then(() => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
-});
+};
+// start Apollo server
+startApolloServer();
