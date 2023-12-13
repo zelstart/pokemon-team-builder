1cb8c8da43ab7ff94617a3af2bc749efb1da3e29
zelstart <e.pomeroy95@gmail.com>
2023-12-13 18:31:08 -0500

    prettied up the page

--- client/src/components/z_pages/TeamView/TeamView.jsx
+++ client/src/components/z_pages/TeamView/TeamView.jsx
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import PokemonCard from '../../PokemonCard/PokemonCard.jsx';
 import { QUERY_SINGLE_TEAM } from '../../utils/queries';
+import { Row, Col } from 'react-bootstrap';
 
 const TeamView = () => {
     const { id } = useParams();
@@ -25,27 +26,30 @@ const TeamView = () => {
     if (error) return <p>Error :(</p>;
 
     return (
-        <div className='mx-5 mt-2' >
+        <div className='mx-5 mt-2'>
             <h1>{data.team.name}</h1>
-            {data.team.pokemon.map((pokemon, index) => {
-                const { move_1, move_2, move_3, move_4, nature, level, types, sprite, ability } = pokemon;
-                const moves = [move_1, move_2, move_3, move_4];
-                console.log(data);
-                return (
-                    <PokemonCard
-                        key={index}
-                        updateTeamMember={updateTeamMember}
-                        moves={moves}
-                        nature={nature}
-                        level={level}
-                        types={types}
-                        sprite={sprite}
-                        ability={ability}
-                    />
-                );
-            })}
+            <Row>
+                {data.team.pokemon.map((pokemon, index) => {
+                    const { move_1, move_2, move_3, move_4, nature, level, types, sprite, ability } = pokemon;
+                    const moves = [move_1, move_2, move_3, move_4];
+                    console.log(data);
+                    return (
+                        <Col className='mb-3' sm={4} key={index}>
+                            <PokemonCard
+                                updateTeamMember={updateTeamMember}
+                                moves={moves}
+                                nature={nature}
+                                level={level}
+                                types={types}
+                                sprite={sprite}
+                                ability={ability}
+                            />
+                        </Col>
+                    );
+                })}
+            </Row>
         </div>
     );
-};
+            }
 
 export default TeamView;
