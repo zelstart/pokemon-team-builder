093b9e6d7825295a345056fe3f980769b2c1ce1f
zelstart <e.pomeroy95@gmail.com>
2023-12-12 21:09:52 -0500

    trying apollo-server-express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServer } = require('apollo-server-express'); // we're going to switch to using apollo-server-express
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const cors = require('cors');
@@ -9,39 +8,43 @@ const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 // start Apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
     const token = req.headers.authorization || '';
+    console.log("Token: ", token);
     return { token };
   },
-
   cors: {
     origin: '*', // Allow all origins
     credentials: true // Credentials are allowed
   }
 });
+
 const startApolloServer = async () => {
   await server.start();
   app.use(cors());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+
+  // Apply the Apollo server as middleware to the express instance
+  server.applyMiddleware({ app, path: '/graphql', cors: false }); 
+
   app.use(routes);
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`); 
     });
   });
 };
+
 // start Apollo server
 startApolloServer();
