1c8e6c340666409a5370178272b0111b212bda4b
zelstart <e.pomeroy95@gmail.com>
2023-12-12 19:40:23 -0500

    added the if back in

--- server/server.js
+++ server/server.js
@@ -27,14 +27,14 @@ const startApolloServer = async () => {
   app.use('/graphql', expressMiddleware(server));
   app.use(routes);
   // if we're in production, serve client/dist as static assets
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   
-  //   // add a wildcard route to serve up the client's index.html file
-  //   app.get('*', (req, res) => {
-  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  //   });
-  // }
+    // add a wildcard route to serve up the client's index.html file
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
   db.once('open', () => {
     app.listen(PORT, () => {
