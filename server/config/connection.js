a7d711f5591a3f93f03c18bbbe71e6a2cf4cf8ec
zelstart <e.pomeroy95@gmail.com>
2023-12-13 16:59:04 -0500

    updated connection for mongo atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams');
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pokemon-teams';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Database connected successfully'))
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
