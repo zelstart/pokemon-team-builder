25f6048b488764c6fc2e7533c9c9b171eb394999
zelstart <e.pomeroy95@gmail.com>
2023-12-14 15:13:08 -0500

    Update ApolloServer configuration and add database connection

--- netlify/functions/graphql.js
+++ netlify/functions/graphql.js
@@ -1,10 +1,26 @@
 const { ApolloServer, gql } = require('apollo-server-lambda');
-const { resolvers } = require('../../server/schemas/resolvers');
-const { typeDefs } = require('../../server/schemas/typeDefs');
+const mongoose = require('mongoose');
+const { typeDefs, resolvers } = require('../../server/schemas');
+
+let cachedDb = null;
+
+async function connectToDatabase() {
+    if (cachedDb) {
+        console.log('Using cached database instance');
+        return Promise.resolve(cachedDb);
+    }
+
+    return mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(db => {
+            cachedDb = db;
+            return cachedDb;
+        });
+}
 
 const server = new ApolloServer({ typeDefs, resolvers });
+
 exports.handler = async (event, context) => {
-  console.log('Event:', event);
-  console.log('Context:', context);
-  return server.createHandler()(event, context);
+    context.callbackWaitsForEmptyEventLoop = false;
+    await connectToDatabase();
+    return server.createHandler()(event, context);
 };
