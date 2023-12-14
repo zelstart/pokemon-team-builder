82daa65a042ff46a471b259eb34fcf8ac651caec
zelstart <e.pomeroy95@gmail.com>
2023-12-14 15:06:19 -0500

    adding console logs

--- netlify/functions/graphql.js
+++ netlify/functions/graphql.js
@@ -3,4 +3,8 @@ const { resolvers } = require('../../server/schemas/resolvers');
 const { typeDefs } = require('../../server/schemas/typeDefs');
 
 const server = new ApolloServer({ typeDefs, resolvers });
-exports.handler = server.createHandler();
+exports.handler = async (event, context) => {
+  console.log('Event:', event);
+  console.log('Context:', context);
+  return server.createHandler()(event, context);
+};
